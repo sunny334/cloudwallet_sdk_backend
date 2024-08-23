d9ff378b50beb1406968d3d474abb54b3064b26d
usamablocksdev <usama@blocksdev.com>
2024-08-23 10:48:10 +0500

    Basic Auth

---
+++ routes/basic_auth.js
@@ -0,0 +1,74 @@
+var express = require("express");
+require("../db")();
+const { generateKey } = require("./utils");
+const { signToken } = require("../auth/utils");
+const { logger } = require("../bin/winstonLogger");
+const { toUtf8 } = require("@cosmjs/encoding");
+const { DirectSecp256k1Wallet } = require("@cosmjs/proto-signing");
+const { default: axios } = require("axios");
+
+const { TWILIO_SERVICE_SID, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } =
+  process.env;
+
+const client = require("twilio")(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, {
+  lazyLoading: true,
+});
+
+var router = express.Router();
+router.get("/basic-auth", async function (req, res) {
+  try {
+    res.render("basic-auth/basic-auth");
+  } catch (e) {
+    logger(e.message);
+  }
+});
+
+router.get("/verify-user", async function (req, res, next) {
+  try {
+    const profile = await axios.post("", {
+      email: req.body.email,
+      password: req.body.password,
+    }); //URL
+
+    profile = {
+      email: profile.email,
+      id: profile.hash,
+    };
+
+    const row = await generateKey(profile, "basic-auth");
+
+    if (!row) {
+      res
+        .status(error?.status || 400)
+        .send(error?.message || "Something went wrong!");
+    } else {
+      const user = {
+        id: row.id,
+        email: "",
+        profileId: row.phone,
+        displayName: row.displayName,
+        wallet: row.wallet,
+        salt: row.salt,
+        privateKey: row.privateKey,
+      };
+      const token = await signToken(user);
+      res.cookie("jwt", token, {
+        httpOnly: true,
+      });
+      const privateKey = toUtf8(row.privateKey);
+      const wallet = await DirectSecp256k1Wallet.fromKey(privateKey, "juno");
+      let account = await wallet.getAccounts().then((res) => {
+        return res[0]?.address;
+      });
+      const cookie = token;
+      return res.render("index", { user: user, account, cookie });
+    }
+  } catch (error) {
+    logger(error.message);
+    res
+      .status(error?.status || 400)
+      .send(error?.message || "Something went wrong!");
+  }
+});
+
+module.exports = router;
--- routes/utils.js
+++ routes/utils.js
@@ -1,68 +1,79 @@
-const {DirectSecp256k1Wallet} = require("@cosmjs/proto-signing");
-const {toUtf8} = require("@cosmjs/encoding");
-const {User} = require("../models/User");
-const {logger} = require("../bin/winstonLogger");
+const { DirectSecp256k1Wallet } = require("@cosmjs/proto-signing");
+const { toUtf8 } = require("@cosmjs/encoding");
+const { User } = require("../models/User");
+const { logger } = require("../bin/winstonLogger");
 
-const generateKey = async (profile, provider = 'google') => {
-    try {
-        let email = '';
-        let exist = false;
-        let obj = {};
-        if (provider === 'sms') {
-            const profileId = profile.id
-            exist = await User.findOne({profileId, provider});
-            obj = {countryCode: profile.countryCode, phone: profile.phone}
-        } else {
-            email = profile.emails[0].value
-            exist = await User.findOne({email, provider});
-        }
-        if (!exist) {
-            let randLength = (33 - profile.id.length).toString();
-            let rand = Math.floor(Math.random() * "1".padEnd(randLength, "0"));
-            if(`${profile.id + rand}`.length < 32){
-                randLength = (33 - profile.id.length).toString();
-                rand = Math.floor(Math.random() * "1".padEnd(randLength, "0"));
-            }
-            const privateKey = toUtf8(profile.id + rand);
-            const wallet = await DirectSecp256k1Wallet.fromKey(privateKey, process.env.CHAIN_PREFIX ?? 'juno');
-            let account = await wallet.getAccounts().then((res) => {
-                return res[0]?.address;
-            });
-            let user = new User({
-                ...obj,
-                email: email,
-                salt: rand,
-                displayName: profile?.displayName,
-                walletAddress: account,
-                provider,
-            });
-            user = await user.save();
-            user["wallet"] = wallet;
-            user["privateKey"] = profile.id + rand;
-            /**
-             * Faucet tokens
-             */
-            // await axios
-            //     .post(process.env.FAUCET_URL + "/api/faucet", {
-            //         receipent: account,
-            //     })
-            //     .catch((e) => console.error(e));
-
-            return user;
-        } else {
-            const privateKey = toUtf8(profile.id + exist.salt);
-            const wallet = await DirectSecp256k1Wallet.fromKey(privateKey, process.env.CHAIN_PREFIX ?? 'juno');
-            exist["wallet"] = wallet;
-            exist["privateKey"] = profile.id + exist.salt;
-            exist["profileId"] = profile.id;
-            exist["email"] = email;
-            return exist;
-        }
-    } catch (e) {
-        logger(e.message);
-        console.error("error while generating token",e.message)
-        return undefined;
+const generateKey = async (profile, provider = "google") => {
+  try {
+    let email = "";
+    let exist = false;
+    let obj = {};
+    if (provider === "sms") {
+      const profileId = profile.id;
+      exist = await User.findOne({ profileId, provider });
+      obj = { countryCode: profile.countryCode, phone: profile.phone };
     }
+    if (provider === "basic-auth") {
+      const profileId = profile.email;
+      exist = await User.findOne({ profileId, provider });
+      email = profile.email;
+    } else {
+      email = profile.emails[0].value;
+      exist = await User.findOne({ email, provider });
+    }
+    if (!exist) {
+      let randLength = (33 - profile.id.length).toString();
+      let rand = Math.floor(Math.random() * "1".padEnd(randLength, "0"));
+      if (`${profile.id + rand}`.length < 32) {
+        randLength = (33 - profile.id.length).toString();
+        rand = Math.floor(Math.random() * "1".padEnd(randLength, "0"));
+      }
+      const privateKey = toUtf8(profile.id + rand);
+      const wallet = await DirectSecp256k1Wallet.fromKey(
+        privateKey,
+        process.env.CHAIN_PREFIX ?? "juno"
+      );
+      let account = await wallet.getAccounts().then((res) => {
+        return res[0]?.address;
+      });
+      let user = new User({
+        ...obj,
+        email: email,
+        salt: rand,
+        displayName: profile?.displayName,
+        walletAddress: account,
+        provider,
+      });
+      user = await user.save();
+      user["wallet"] = wallet;
+      user["privateKey"] = profile.id + rand;
+      /**
+       * Faucet tokens
+       */
+      // await axios
+      //     .post(process.env.FAUCET_URL + "/api/faucet", {
+      //         receipent: account,
+      //     })
+      //     .catch((e) => console.error(e));
+
+      return user;
+    } else {
+      const privateKey = toUtf8(profile.id + exist.salt);
+      const wallet = await DirectSecp256k1Wallet.fromKey(
+        privateKey,
+        process.env.CHAIN_PREFIX ?? "juno"
+      );
+      exist["wallet"] = wallet;
+      exist["privateKey"] = profile.id + exist.salt;
+      exist["profileId"] = profile.id;
+      exist["email"] = email;
+      return exist;
+    }
+  } catch (e) {
+    logger(e.message);
+    console.error("error while generating token", e.message);
+    return undefined;
+  }
 };
 
 module.exports.generateKey = generateKey;
