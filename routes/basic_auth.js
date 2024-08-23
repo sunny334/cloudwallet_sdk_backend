d5adaaba829831c3ad9e7220216af91ae3dbe8e8
usamablocksdev <usama@blocksdev.com>
2024-08-23 16:42:11 +0500

    Added https

--- routes/basic_auth.js
+++ routes/basic_auth.js
@@ -35,7 +35,7 @@ router.get("/reset", async function (req, res) {
 router.get("/verify-user/:id", async function (req, res, next) {
   try {
     let profile = await axios.get(
-      `cloud-wallet-basic-auth.cosmichub.store/user/get-user/${req.params.id}`
+      `https://cloud-wallet-basic-auth.cosmichub.store/user/get-user/${req.params.id}`
     );
 
     console.log("Profile", profile.data);
